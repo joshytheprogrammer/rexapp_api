db1159ababb8ca27f34a877d7184a974ce95e9cf
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-23 16:06:28 +0100

    'made changes: 'added changes email route'

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const validateToken = require('../middleware/validateToken');
 
 // Apply validateToken middleware to routes that require authentication
@@ -32,6 +34,36 @@ router.post('/change-username', validateToken, async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'An error occurred while updating the username' });
+  }
+});
+
+router.post('/change-email', validateToken, async (req, res) => {
+  const newEmail = req.body.newEmail;
+
+  // Validate new email
+  if (!newEmail) {
+    return res.status(400).json({ message: 'New email is required!' });
+  }
+
+  // Check if the new email is already taken
+  const emailExists = await User.findOne({ email: newEmail });
+  if (emailExists) {
+    return res.status(400).json({ message: 'Email already exists' });
+  }
+
+  // Update the email in the database
+  try {
+    const userId = req.user.id; // Assuming you're using validateToken middleware
+    const user = await User.findByIdAndUpdate(userId, { email: newEmail }, { new: true });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json({ message: 'Email updated successfully', user });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'An error occurred while updating the email' });
   }
 });
 
