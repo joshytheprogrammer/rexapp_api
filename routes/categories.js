c296f4311b25db6dc0d798f9be7f628c93c200b3
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-09-02 17:54:40 +0100

    made changes

--- routes/categories.js
+++ routes/categories.js
@@ -2,32 +2,45 @@ const router = require('express').Router();
 const Category = require('../models/Category');
 const Search = require('../models/Search');
 
-router.get('/all', async (req, res) => {
-  // GET /api/categories/all?filters={"name": "Electronics"}&limit=10&fields=_id,name&sort=name
-  try {
-    let query = Category.find();
 
+router.get('/all', async (req, res) => {
+  try {
+    // Define the initial aggregation pipeline
+    const aggregationPipeline = [];
+
+    // Check if filters are provided
     if (req.query.filters) {
       const filters = JSON.parse(req.query.filters);
-      query = query.where(filters);
+      aggregationPipeline.push({ $match: filters });
     }
 
+    // Check if fields are provided
     if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
+      const fields = req.query.fields.split(',').reduce((accumulator, field) => {
+        accumulator[field] = 1;
+        return accumulator;
+      }, {});
+      aggregationPipeline.push({ $project: fields });
     }
 
+    // Check if sort is provided
     if (req.query.sort) {
       const sortField = req.query.sort;
-      query = query.sort(sortField);
+      aggregationPipeline.push({ $sort: { [sortField]: 1 } });
     }
 
-    let limit = parseInt(req.query.limit) || null;
-    if (limit !== null) {
-      query = query.limit(limit);
+    // Parse and validate the limit
+    let limit = parseInt(req.query.limit, 10);
+    if (isNaN(limit) || limit <= 0) {
+      limit = 10; // Default limit if not provided or invalid
     }
 
-    const categories = await query.exec();
+    // Add $sample stage to get random documents
+    aggregationPipeline.push({ $sample: { size: limit } });
+
+    // Execute the aggregation pipeline
+    const categories = await Category.aggregate(aggregationPipeline);
+
     return res.status(200).json({ categories });
   } catch (error) {
     console.error(error);
