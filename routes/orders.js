5fec06e5d315a70b762ffe0bf9fd89e38a581121
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-09-09 11:38:45 +0100

    made changes: ''

--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
 const User = require('../models/User');
 const validateToken = require('../middleware/validateToken');
 
@@ -31,5 +32,35 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: 'An error occurred while fetching orders.' });
   }
 });
+
+router.get('/byId/:id', async (req, res) => {
+  const userId = req.user.id;
+  const orderId = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(orderId)) {
+    return res.status(400).json({ message: 'Invalid order ID.' });
+  }
+
+  try {
+    const user = await User.findById(userId); 
+
+    if (!user) {
+      return res.status(400).json({ message: 'User not found.' });
+    }
+
+    const order = user.orders.find(order => order._id.toString() === orderId);
+
+    if (!order) {
+      return res.status(400).json({ message: 'Order not found.' });
+    }
+
+    // Return the found order
+    return res.status(200).json(order);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'An error occurred while fetching the order.' });
+  }
+});
+
 
 module.exports = router;
